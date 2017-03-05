61369e16456dd533645c17f71b3b8e50a531ff27
Recca Tsai <recca0120.gmail.com>
2017-03-05 22:45:05 +0800

    doc

--- src/JString/Javascript.php
+++ src/JString/Javascript.php
@@ -219,7 +219,7 @@ trait Javascript
     /**
      * The replace() method returns a new string with some or all matches of a pattern replaced by a replacement. The pattern can be a string or a RegExp, and the replacement can be a string or a function to be called for each match.
      *
-     * @param string $regexp
+     * @param mixed $regexp
      * @param mixed $replacement
      * @return static
      */
